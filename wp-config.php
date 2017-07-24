2de2e221d71bd6308db181eb09313df0cdc271bf
tien <tienls@fastboy.net>
2017-07-24 20:33:59 +0700

    Edit wp-congif.php

--- wp-config.php
+++ wp-config.php
@@ -78,6 +78,8 @@ $table_prefix  = 'wp_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
+define('WP_HOME','https://learning-wp.000webhostapp.com/'); 
+define('WP_SITEURL','https://learning-wp.000webhostapp.com/');
 
 /* That's all, stop editing! Happy blogging. */
 
