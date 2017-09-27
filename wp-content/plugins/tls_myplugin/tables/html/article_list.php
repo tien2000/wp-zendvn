36cd4243c729c5cfdce8915a053bb128d777d75c
tien <tienls@fastboy.net>
2017-09-27 18:45:10 +0700

    WP_List_Table || Edit, Delete, Status action | Build get_fun(), status(), delete_data()

--- wp-content/plugins/tls_myplugin/tables/html/article_list.php
+++ wp-content/plugins/tls_myplugin/tables/html/article_list.php
@@ -4,10 +4,13 @@
     /* echo '<pre>';
     print_r($tblArticle);
     echo '</pre>'; */
+    $page = @$_REQUEST['page'];
 ?>
 
 <div class="wrap">
-	<h1>Articles</h1>	
-	<?php $tblArticle->search_box('Search Articles', 'search-id');?>
-	<?php $tblArticle->display();?>
+	<h1>Articles</h1>
+	<form action="" method="post" name="<?php echo $page;?>" id="<?php echo $page;?>">
+    	<?php $tblArticle->search_box('Search Articles', 'search-id');?>
+    	<?php $tblArticle->display();?>
+	</form>
 </div>
--- wp-content/plugins/tls_myplugin/tables/my_article.php
+++ wp-content/plugins/tls_myplugin/tables/my_article.php
@@ -28,17 +28,25 @@
         public function article_menu(){
             //echo '<br>' . __METHOD__;
             $action = @$_REQUEST['action'];
-            $func = 'display';
-            
-            if($action == 'edit'){
-                $func = 'display_edit';
-            }
+            $func = $this->get_func();
             
             add_menu_page('Articles', 'Articles', 'manage_options', 
                             $this->_menuSlug, array($this, $func), '', 3);
             
             add_submenu_page($this->_menuSlug, 'Add New', 'Add New', 'manage_options', 
                                 $this->_menuSlug . '-add', array($this, 'display_add'));
+        }
+
+        private function get_func(){
+            $action = @$_REQUEST['action'];
+            switch ($action){
+                case 'edit'     :return 'display_edit';
+                case 'delete'   :return 'delete_data';
+                case 'inactive' :return 'status';
+                case 'active'   :return 'status';
+                
+                default         :return 'display';
+            }
         }
         
         public function display(){
@@ -107,6 +115,65 @@
                 }
             }
             require_once TLS_PLUGIN_TABLE_DIR . 'html/article_form.php';            
+        }
+        
+        public function status(){
+            global $wpdb;            
+            $article_id     = @$_REQUEST['article'];            
+            $status         = (@$_REQUEST['action'] == 'inactive')?0:1;
+            $paged          = max(1, @$_REQUEST['paged']);
+            $table          = $wpdb->prefix . 'mp_article';
+            
+            if(!is_array($_REQUEST['article'])){
+                
+                $data           = array( 'status' => $status );
+                $where          = array( 'id' => $article_id );
+                $format         = array('%d');
+                $where_format   = array('%d');
+                $wpdb->update($table, $data, $where, $format, $where_format);                
+            }else{
+                $ids = join(',', $_REQUEST['article']);
+                $sql = 'UPDATE '. $table .' SET status='. $status .' WHERE id IN ('. $ids .') ';
+                $wpdb->query($sql);
+            }
+            
+            $url = 'admin.php?page=' . @$_REQUEST['page'] . '&paged=' . $paged . '&mes=1';
+            wp_redirect($url);
+            
+            /* echo '<pre>';
+            print_r($_POST);
+            echo '</pre>'; */
+        }
+        
+        public function delete_data(){
+            //echo '<br>' . __METHOD__;
+            
+            global $wpdb;
+            $table = $wpdb->prefix . 'mp_article';
+            $article_id = @$_REQUEST['article'];
+            //echo '<br>' . $article_id;   
+            
+            if(!is_array($_REQUEST['article'])){
+                $where = array('id' => $article_id);
+                $where_format = array('%d');
+                
+                $wpdb->delete($table, $where, $where_format);
+            }else{
+                $security_code = @$_REQUEST['security_code'];    // Kiểm tra dữ liệu POST qua
+                if(wp_verify_nonce($security_code, 'delete')){
+                    $ids = join(',', $_REQUEST['article']);
+                    $sql = 'DELETE FROM '. $table .' WHERE id IN ('. $ids .') ';
+                    //echo '<br>' . $sql;
+                    $wpdb->query($sql);
+                }                
+            }
+            
+            $url = 'admin.php?page=' . @$_REQUEST['page'] . '&mes=1';
+            wp_redirect($url);
+            
+            /* echo '<pre>';
+            print_r($_POST);
+            echo '</pre>'; */
         }
         
         private function save_data($action = 'add'){
