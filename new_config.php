d7c929017b25f3cc946a5d143db04cc20c53b270
tien <tienls@fastboy.net>
2017-10-11 16:27:59 +0700

    Edit file new_config.php

--- new_config.php
+++ new_config.php
@@ -1,10 +1,12 @@
 <?php
 define('SAVEQUERIES', true);
 
+// Tắt cảnh báo trên hệ thống WP.
 define('WP_DEBUG_DISPLAY', false);
-@ini_set('display_errors', 0);
+@ini_set('display_errors', 0);      // Sử dụng khi dòng trên ko hoạt động
 
-define('SCRIPT_DEBUG', true);
+// Debug cho CSS ,JS
+define('SCRIPT_DEBUG', true);       // Khi có lỗi sẽ ghi vào tập tin logs
 define('WP_DEBUG_LOG', true);
 
 define('WP_POST_REVISIONS', 3);     // Giới hạn lưu Previuos Post trong DB.
